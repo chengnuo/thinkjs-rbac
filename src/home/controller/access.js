3b00152ce9086cd01ff8ba7ef2eafa2e7b45c453
PC-201701072109\Administrator <2585441871@qq.com>
2017-02-19 01:29:59 +0800

    用户管理，角色管理，权限管理。都做完了，下面把功能关联起来。

--- src/home/controller/access.js
+++ src/home/controller/access.js
@@ -7,12 +7,24 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  indexAction(){
-    //auto render template file index_index.html
-    return this.display();
-  }
+   async indexAction(){
+     let result=await this.model("access").limit(10).select();
+     this.assign("roleList",result)
+     //auto render template file index_index.html
+     return this.display();
+   }
   setAction(){
     //auto render template file index_index.html
     return this.display();
+  }
+  async dosetAction(){
+      let data=this.post();
+      let model = this.model("access");
+      let insertId = await model.add({
+          id:data.id,
+          title:data.title,
+          urls:data.urls
+        });
+      console.log("insertId",insertId)
   }
 }
--- src/home/controller/user.js
+++ src/home/controller/user.js
@@ -7,10 +7,12 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  indexAction(){
-    //auto render template file index_index.html
-    return this.display();
-  }
+   async indexAction(){
+     let result=await this.model("user").limit(10).select();
+     this.assign("roleList",result)
+     //auto render template file index_index.html
+     return this.display();
+   }
   setAction(){
     //auto render template file index_index.html
     return this.display();
