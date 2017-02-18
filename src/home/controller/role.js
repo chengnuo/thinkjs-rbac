be87a0fcc1c98a5b1dcd7aaa79dada2eb7ebcab3
PC-201701072109\Administrator <2585441871@qq.com>
2017-02-19 00:31:42 +0800

    角色管理，做的不是很好。等待优化，先做出功能

--- src/home/controller/role.js
+++ src/home/controller/role.js
@@ -7,16 +7,30 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  indexAction(){
+  async indexAction(){
+
+
+
+    let result=await this.model("role").limit(10).select();
+
+
+    this.assign("roleList",result)
     //auto render template file index_index.html
     return this.display();
   }
-  setAction(){
+  async setAction(){
     //auto render template file index_index.html
     return this.display();
+  }
+  async dosetAction(){
+      let data=this.post();
+      let model = this.model("role");
+      let insertId = await model.add({id:data.id, name:data.name});
+      console.log("insertId",insertId)
   }
   accessAction(){
     //auto render template file index_index.html
     return this.display();
   }
+
 }
